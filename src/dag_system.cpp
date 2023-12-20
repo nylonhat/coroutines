8a6fb84da70a5448d736854b6517c968f5647344
nylonhat <nylonhat@gmail.com>
2023-12-20 18:03:01 +1030

    refactor

--- src/dag_system.cpp
+++ src/dag_system.cpp
@@ -34,7 +34,7 @@ Task<size_t> DagSystem::benchmark(int iterations){
 
 	for (size_t i=0; i< iterations; i++){
 		//result = fib_f(50);
-		result = co_await fib(50);
+		result = co_await fib(55);
 		//result += co_await multiply(i,1);
 		//result += co_await threadpool.chain(multiply(i,1));
 		//result += co_await co_await threadpool.branch(multiply(i,1));
--- src/deque.h
+++ src/deque.h
@@ -13,85 +13,8 @@ requires
 	(std::has_single_bit(buffer_size)) //power of 2
 
 class Deque{
-
-public:
-	Deque(){
-		for(size_t i = 0; i < buffer_size; i++){
-			buffer[i].sequence.store(i, relaxed);
-		}
-	}
-	
-	~Deque() = default;
-
-
-	bool try_local_push(T const& data){
-		Cell& cell = buffer[stack_id & buffer_mask];
-		size_t seq = cell.sequence.load(acquire);
-		
-		if(seq != stack_id){
-			return false;
-		}
-
-		cell.data = data;
-		cell.sequence.store(stack_id + 1, release);
-		stack_id++;
-		return true;
-	}
-
-	bool try_local_pop(T& data){
-		Cell& cell = buffer[(stack_id - 1) & buffer_mask];
-
-		size_t old_seq = stack_id;
-		if(!cell.sequence.compare_exchange_strong(old_seq, stack_id - 1, acq_rel)){
-			return false;
-		}
-
-		size_t old_steal = stack_id - 1;
-		if(steal_id.compare_exchange_strong(old_steal, stack_id, relaxed)){
-			data = cell.data;
-			cell.sequence.store(old_steal + buffer_size, release);
-			return true;
-		}
-
-		if(old_steal == stack_id){
-			return false;
-		}
-
-		data = cell.data;
-		stack_id--;
-		return true;
-	}
-
-	bool try_steal(T& data){
-		Cell* cell;
-		size_t old_steal = steal_id.load(relaxed);
-
-		for(;;){
-			cell = &buffer[old_steal & buffer_mask];
-			size_t seq = cell->sequence.load(acquire);
-
-			intptr_t dif = (intptr_t)seq - (intptr_t)(old_steal + 1);
-
-			if(dif < 0){
-				return false;
-			}
-			
-			if(dif > 0){
-				old_steal = steal_id.load(relaxed);
-				continue;
-			}
-
-			if(steal_id.compare_exchange_weak(old_steal, old_steal + 1, relaxed)){
-				break;
-			}
-		}
-
-		data = cell->data;
-		cell->sequence.store(old_steal + buffer_size, release);
-		return true;
-	}
-
 private:
+	using m = std::memory_order;
 
 	struct alignas(64) Cell{
 		std::atomic<size_t> sequence;
@@ -106,10 +29,83 @@ private:
 	Deque(Deque const&) = delete;
 	void operator= (Deque const&) = delete;
 
-	static constexpr auto acquire = std::memory_order::acquire;
-	static constexpr auto release = std::memory_order::release;
-	static constexpr auto acq_rel = std::memory_order::acq_rel;
-	static constexpr auto relaxed = std::memory_order::relaxed;
+public:
+	Deque(){
+		for(size_t i = 0; i < buffer_size; i++){
+			buffer[i].sequence.store(i, m::relaxed);
+		}
+	}
+	
+	~Deque() = default;
+
+
+	bool try_local_push(T const& data){
+		Cell& cell = buffer[stack_id & buffer_mask];
+		size_t seq = cell.sequence.load(m::acquire);
+		
+		if(seq != stack_id){
+			return false;
+		}
+
+		cell.data = data;
+		cell.sequence.store(stack_id + 1, m::release);
+		stack_id++;
+		return true;
+	}
+
+	bool try_local_pop(T& data){
+		Cell& cell = buffer[(stack_id - 1) & buffer_mask];
+
+		size_t old_seq = stack_id;
+		if(!cell.sequence.compare_exchange_strong(old_seq, stack_id - 1, m::acq_rel)){
+			return false;
+		}
+
+		size_t old_steal = stack_id - 1;
+		if(steal_id.compare_exchange_strong(old_steal, stack_id, m::relaxed)){
+			data = cell.data;
+			cell.sequence.store(old_steal + buffer_size, m::release);
+			return true;
+		}
+
+		if(old_steal == stack_id){
+			return false;
+		}
+
+		data = cell.data;
+		stack_id--;
+		return true;
+	}
+
+	bool try_steal(T& data){
+		Cell* cell;
+		size_t old_steal = steal_id.load(m::relaxed);
+
+		for(;;){
+			cell = &buffer[old_steal & buffer_mask];
+			size_t seq = cell->sequence.load(m::acquire);
+
+			intptr_t dif = (intptr_t)seq - (intptr_t)(old_steal + 1);
+
+			if(dif < 0){
+				return false;
+			}
+			
+			if(dif > 0){
+				old_steal = steal_id.load(m::relaxed);
+				continue;
+			}
+
+			if(steal_id.compare_exchange_weak(old_steal, old_steal + 1, m::relaxed)){
+				break;
+			}
+		}
+
+		data = cell->data;
+		cell->sequence.store(old_steal + buffer_size, m::release);
+		return true;
+	}
+
 };
 
 #endif
