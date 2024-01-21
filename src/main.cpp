2bc2cbd39e717bdb7a22c84de5420ff5a040a69e
nylonhat <nylonhat@gmail.com>
2024-01-21 21:58:18 +1030

    monadic complete

--- src/main.cpp
+++ src/main.cpp
@@ -4,30 +4,6 @@
 #include "monadic.h"
 #include <tuple>
 
-Task<int> mult(int a, int b){
-	co_return a*b;
-}
-
-Task<float> multf(float a, float b){
-	co_return a*b;
-}
-
-int sub(int a, int b){
-	return a-b;
-}
-
-
-
-Sync<int> maincoro(){
-	auto make_tup = [](auto&&... args){
-		return std::make_tuple(args...);	
-	};
-
-	auto [a, b] = co_await fmap(make_tup, mult(7,5), multf(3.41,1.44));
-	std::cout << a << " " << b <<  "\n";
-	co_return 0;
-}
-
 int main() {
 	
 /*
@@ -44,10 +20,9 @@ int main() {
 //	IOSystem io_system{};
 //	io_system.entry().await();
 
-//	DagSystem dag_system;
-//	dag_system.entry().await();
+	DagSystem dag_system;
+	dag_system.entry().await();
 
-	maincoro().await();
 
 	return 0;
 
